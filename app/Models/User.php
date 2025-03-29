3fb69273c7795a9dbe111f9755232fcf50f6d621
Sralud <llorente.gerald2@gmail.com>
2025-03-29 08:22:26 +0800

    updated files for site 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,18 +1,13 @@
 <?php
-    
-    namespace App\Models;
-    
-    use Illuminate\Database\Eloquent\Model;
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
     class User extends Model{
         protected $table = 'tbluser';
         // column sa table
-        protected $fillable = [
-            'username', 'password', 'gender'
-        ];
+        protected $fillable = ['username', 'password','gender'];
         public $timestamps = false;
         protected $primaryKey = 'id';
-
-        protected $hidden = [
-            'password',
-        ];
  }
